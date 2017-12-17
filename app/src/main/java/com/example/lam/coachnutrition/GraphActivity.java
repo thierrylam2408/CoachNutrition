b612aceea1fa57d20abb730f4639c99c7efbe459
thierrylam2408 <thierrylam2408@gmail.com>
2017-12-17 21:42:08 +0100

    preferences Calories

--- app/src/main/java/com/example/lam/coachnutrition/GraphActivity.java
+++ app/src/main/java/com/example/lam/coachnutrition/GraphActivity.java
@@ -1,9 +1,16 @@
 package com.example.lam.coachnutrition;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.LegendRenderer;
@@ -20,17 +27,12 @@ import java.util.Date;
 public class GraphActivity extends AppCompatActivity {
 
     private AccessProvider accessProvider;
-    private final String[] select = {
-            BaseInformation.MealEntry._ID,
-            BaseInformation.MealEntry.COLUMN_CODE,
-            BaseInformation.MealEntry.COLUMN_NAME,
-            BaseInformation.MealEntry.COLUMN_TIMESTAMP,
-            BaseInformation.MealEntry.COLUMN_FOOD,
-            BaseInformation.MealEntry.COLUMN_WEIGHT};
     private Calendar cal;
     private GraphView graph;
-    private float minCal=350F;
-    private float maxCal=820F;
+    private float minCal;
+    private float maxCal;
+    private EditText editMin;
+    private EditText editMax;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +44,23 @@ public class GraphActivity extends AppCompatActivity {
         cal.set(Calendar.HOUR_OF_DAY, 0);
         cal.set(Calendar.MINUTE, 0);
         cal.set(Calendar.SECOND, 0);
+        editMin = (EditText) findViewById(R.id.editMin);
+        editMax = (EditText) findViewById(R.id.editMax);
 
-        barCalories();
-        drawLimit(minCal);
-        drawLimit(maxCal);
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        minCal = sharedPref.getFloat("minCal", 2150);
+        maxCal = sharedPref.getFloat("maxCal", 2800F);
+        editMin.setHint(String.valueOf(minCal));
+        editMax.setHint(String.valueOf(maxCal));
+        setListener();
 
+
+
+        drawGraph();
+        settingGraph();
+    }
+
+    private void settingGraph() {
         graph.setTitle("Objectif hebdomadaire");
         graph.getGridLabelRenderer().setLabelFormatter(
                 new DateAsXAxisLabelFormatter(getApplication(), new SimpleDateFormat("dd/MM/yy")));
@@ -59,9 +73,16 @@ public class GraphActivity extends AppCompatActivity {
         graph.getGridLabelRenderer().setHumanRounding(false);
     }
 
-    private void barCalories(){
+    private void drawGraph() {
+        graph.removeAllSeries();
+        barCalories();
+        drawLimit(minCal);
+        drawLimit(maxCal);
+    }
+
+    private void barCalories() {
         DataPoint[] datas = new DataPoint[7];
-        for (int i=0; i<7; i++){
+        for (int i = 0; i < 7; i++) {
             Date date = new Date();
             date.setTime(cal.getTimeInMillis());
             Float calories = accessProvider.countCalories(cal);
@@ -74,7 +95,7 @@ public class GraphActivity extends AppCompatActivity {
         series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
             @Override
             public int get(DataPoint data) {
-                if(data.getY()> maxCal || data.getY()< minCal)
+                if (data.getY() > maxCal || data.getY() < minCal)
                     return Color.rgb(255, 44, 46);
                 else return Color.rgb(86, 201, 50);
             }
@@ -82,12 +103,12 @@ public class GraphActivity extends AppCompatActivity {
         graph.addSeries(series);
     }
 
-    private void drawLimit(float limit){
+    private void drawLimit(float limit) {
         DataPoint[] tab = new DataPoint[7];
-        for (int i=0; i<7; i++){
+        for (int i = 0; i < 7; i++) {
             Date date = new Date();
             date.setTime(cal.getTimeInMillis());
-            tab[i] = new DataPoint(date,limit);
+            tab[i] = new DataPoint(date, limit);
             cal.add(Calendar.DAY_OF_YEAR, 1);
         }
         cal.add(Calendar.DAY_OF_YEAR, -7);
@@ -95,5 +116,42 @@ public class GraphActivity extends AppCompatActivity {
         line.setThickness(2);
         line.setColor(Color.rgb(46, 93, 248));
         graph.addSeries(line);
+    }
+
+    private void  setListener(){
+        editMin.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!s.toString().trim().equals("")) {
+                    minCal = Float.parseFloat(s.toString());
+                    SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putFloat("minCal", minCal);
+                    editor.commit();
+                    drawGraph();
+                }
+            }
+        });
+        editMax.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!s.toString().trim().equals("")) {
+                    maxCal = Float.parseFloat(s.toString());
+                    SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putFloat("maxCal", maxCal);
+                    editor.commit();
+                    drawGraph();
+                }
+            }
+        });
     }
 }
